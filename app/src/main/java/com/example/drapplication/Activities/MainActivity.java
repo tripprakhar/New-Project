7266d5cb7c882237c43ab1bb61dd1484cc5e1694
Prakhar Tripathi <tripprakhar@gmail.com>
2020-09-25 21:42:13 +0530

    pt1

--- app/src/main/java/com/example/drapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/drapplication/Activities/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.drapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.drapplication.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private Button signinbutton;
+    private Button signupbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signinbutton=(Button) findViewById(R.id.signinId);
+        signupbutton=(Button) findViewById(R.id.signupId);
     }
+    public void gotosignin (View view){
+        Intent intent = new Intent (this,  signInActivity.class);
+        startActivity(intent);
+    }
+    public void gotosignup (View view){
+        Intent intent = new Intent (this,  signUpActivity.class);
+        startActivity(intent);
+    }
+
 }
