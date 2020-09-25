2bdf17bd6b61ebc16b5b23a5d69aa72e3c1e3eb3
yashmodi2911 <yashmodi817@gmail.com>
2020-09-25 20:10:35 +0530

    signIn1

--- app/src/main/java/com/example/drapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/drapplication/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.drapplication;
+package com.example.drapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.drapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
