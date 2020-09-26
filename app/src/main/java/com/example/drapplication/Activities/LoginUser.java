bcde6b799e8e5b3b2d13dc3319092ca77266b197
yashmodi2911 <yashmodi817@gmail.com>
2020-09-26 19:00:00 +0530

    signUpCommit

---
+++ app/src/main/java/com/example/drapplication/Activities/LoginUser.java
@@ -0,0 +1,16 @@
+package com.example.drapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drapplication.R;
+
+public class LoginUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_user);
+    }
+}
---
+++ app/src/main/java/com/example/drapplication/Activities/loginInDr.java
@@ -0,0 +1,16 @@
+package com.example.drapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drapplication.R;
+
+public class loginInDr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_in_dr);
+    }
+}
