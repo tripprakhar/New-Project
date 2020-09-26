8325131f88222ef5aa96586ec0b46b77dc105269
yashmodi2911 <yashmodi817@gmail.com>
2020-09-26 20:00:56 +0530

    loginActivitiesReady

--- app/src/main/java/com/example/drapplication/Activities/signInActivity.java
+++ app/src/main/java/com/example/drapplication/Activities/signInActivity.java
@@ -2,15 +2,48 @@ package com.example.drapplication.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.drapplication.R;
 
+import java.text.DateFormat;
+
 public class signInActivity extends AppCompatActivity {
+    private Button userLogin;
+    private Button DoctorLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        userLogin=(Button)findViewById(R.id.signInasUser);
+        DoctorLogin=(Button)findViewById(R.id.signInasDoctor);
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginInasuser();
+            }
+        });
+        DoctorLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginInasdoctor();
+            }
+        });
+    }
+    public void loginInasuser()
+    {
+        startActivity(new Intent(this,LoginUser.class));
+        finish();
+    }
+    public void loginInasdoctor()
+    {
+        startActivity(new Intent(this,loginInDr.class));
+        finish();
     }
 }
