a543f8476bc8a1d22c5e3c179a0026c60712b21a
yashmodi2911 <yashmodi817@gmail.com>
2020-09-27 23:29:02 +0530

    loginActivitiesReady

---
+++ app/src/main/java/com/example/drapplication/Activities/DoctorSignUP.java
@@ -0,0 +1,16 @@
+package com.example.drapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drapplication.R;
+
+public class DoctorSignUP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_sign_u_p);
+    }
+}
